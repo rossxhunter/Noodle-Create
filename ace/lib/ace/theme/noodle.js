db00d085ecceee3dd96ea283198f79e7e3a25f18
Ross Hunter <ross98hunter@gmail.com>
2017-06-21 19:06:04 +0100

    Reload editor

--- ace/lib/ace/theme/noodle.js
+++
@@ -1,9 +0,0 @@
-define(function(require, exports, module) {
-
-exports.isDark = false;
-exports.cssClass = "ace-noodle";
-exports.cssText = require("../requirejs/text!./noodle.css");
-
-var dom = require("../lib/dom");
-dom.importCssString(exports.cssText, exports.cssClass);
-});
